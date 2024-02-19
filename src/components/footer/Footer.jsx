1e392a0b57484c44eeb39e82b8a4da3f28b704bc
KRISHNA MOHAN <134590019+krishnamidathana@users.noreply.github.com>
2024-02-19 15:55:45 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -26,9 +26,7 @@ const Footer = () => {
         <li>
           <a href="#portfolio">Portfolio</a>
         </li>
-        <li>
-          <a href="#testimonials">Testimonials</a>
-        </li>
+     
         <li>
           <a href="#contact">Contact</a>
         </li>
